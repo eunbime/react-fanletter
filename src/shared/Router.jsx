1b9d903f6344c273adca1d1081cf15c073c68862
eunbime <ebk1014@gmail.com>
2023-11-20 11:01:18 +0900

    Add homepage

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -7,7 +7,7 @@ import DetailLetter from "../pages/DetailLetter";
 
 const Router = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index path="/" element={<Home />} />
